218ba8e0ce6d4ace7289532e6277436e1005aeb4
Aman Chaurasiya <amanc2804@gmail.com>
2025-12-17 22:32:08 +0530

    function scope

--- First_Program/Scopes_in.js
+++ First_Program/Scopes_in.js
@@ -16,3 +16,31 @@ console.log(name); // Accessable
 // 2. Function Scope
 // variable declear inside a function are accessible only within that function.
 // Note
+// a. Function scopes is created only by functions.
+// b. Var is function scoped, not block scoped
+
+function myFunction() {
+  let message = "I can do it";
+  console.log(message);
+}
+myFunction();
+
+//3. Block Scope
+// Variables decleared inside {} uning let or const are block scoped
+if (true) {
+  let city = "Delhi";
+  const country = "India";
+}
+
+// tips
+// Lexical scope
+// Inner function can access variable from their outer scope.
+
+function outer() {
+  let count = 5;
+  function inner() {
+    console.log(count);
+  }
+  inner();
+}
+outer();
